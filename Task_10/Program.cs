13b9294b2c180612b55e41b61d6450f3f88e9240
Ruslan <89604587433@mail.ru>
2022-09-05 22:08:49 +0300

    Вторая цыфра трехзначного числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Console.WriteLine("Hello, World!");
+int Func (int x1)
+{
+    x1=x1/10;
+    x1=x1%10;
+    Console.WriteLine($"Вторая цифра {x1}");
+    return x1;
+}
+Console.WriteLine("Введите трехзначное число");
+int a1=Convert.ToInt32(Console.ReadLine());
+Func(a1);
