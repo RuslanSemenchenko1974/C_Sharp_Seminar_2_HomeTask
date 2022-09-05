f20f6b21c485d17dc0a074cfb0239e481795cec2
Ruslan <89604587433@mail.ru>
2022-09-05 23:01:11 +0300

    Определение выходного

---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿void Func (int x1)
+{
+   if (x1>7||x1==0) Console.WriteLine("Неправильный ввод");
+   else 
+   { 
+    if (x1==7||x1==6) Console.WriteLine("Выходной");
+    else Console.WriteLine("Рабочий день");
+   }
+    
+}
+Console.WriteLine("Введите День Недели");
+int a1=Convert.ToInt32(Console.ReadLine());
+Func(a1);
