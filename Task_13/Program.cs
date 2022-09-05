9b92e1b1201ff7801e0a53bdd408fb71c1a80af1
Ruslan <89604587433@mail.ru>
2022-09-05 22:47:48 +0300

    Трерья цифра заданного числа

---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿int Func (int x1)
+{
+    x1=x1%10;
+    Console.WriteLine($"Третья цифра {x1}");
+    return x1;
+}
+
+Console.WriteLine("Введите  число");
+int a1=Convert.ToInt32(Console.ReadLine());
+if (a1<100) 
+  { Console.WriteLine("Такой цыфры нет");  }
+else 
+{  
+    if (a1>=1000)
+    {
+        while (a1>1000)
+        {
+        a1=a1/10; 
+        }
+    }
+Func(a1);
+}
